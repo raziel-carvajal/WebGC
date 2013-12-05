14fac1f17cef90a3a909ad5b9593730de3f67024
raziel <raziel.carvajal-gomez@inria.fr>
2013-12-05 16:10:25 +0100

    Initial commit

---
+++ Gruntfile.js
@@ -0,0 +1,31 @@
+module.exports = function(grunt) {
+
+  // Project configuration.
+  grunt.initConfig({
+    pkg: grunt.file.readJSON('package.json'),
+    uglify: {
+      options: {
+        banner: '/*! <%= pkg.name %> <%= grunt.template.today("yyyy-mm-dd") %> */\n'
+      },
+      build: {
+        src: 'lib/<%= pkg.name %>.js',
+        dest: 'dist/<%= pkg.name %>.min.js'
+      }
+    },
+    copy: {
+      main: {
+        files: [
+	  {expand: true, cwd: 'lib', src: '*', dest: 'dist/'}
+	]
+      }
+    }
+  });
+
+  // Load the plugin that provides the "uglify" task.
+  grunt.loadNpmTasks('grunt-contrib-uglify');
+
+  grunt.loadNpmTasks('grunt-contrib-copy');
+  // Default task(s).
+  grunt.registerTask('default', ['uglify', 'copy']);
+
+};
