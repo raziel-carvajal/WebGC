6d007fc721291da798586f611c0dd23a97d64542
raziel <raziel.carvajal-gomez@inria.fr>
2014-03-06 14:35:12 +0100

    This commit contains the documentation of some files in this repository and a documented local test of Vicinity. See more details on test/vicinity-local-test/README.md

---
+++ README.md
@@ -0,0 +1,4 @@
+# GossipPeer
+
+## Installation
+  bower install
---
+++ test/vicinity-local-test/vicinity-test.sh
@@ -0,0 +1,95 @@
+#!/bin/bash
+#===============================================================================
+#
+#          FILE:  vicinity-test.sh
+# 
+#         USAGE:  ./vicinity-test.sh 
+#                   <peers>                 - Number of peers in the test (integer)
+#                   <execution-time>        - Duration (in seconds) of the test (integer)
+#                   <serverjs-gossip-dir>   - Directory that contains the fork of the 
+#                                             serverjs-gossip project
+#                   <test-directory>        - Directory where the test will be executed
+# 
+#   DESCRIPTION:  This script coordinates a local test of the Vicinity protocol amog a set
+#               of peers. The representation of each peer is made using a window of the
+#               the google chrome browswer. So, it is recommended to make a test with at
+#               most 15 peers in order to avoid an affectation to the performance of the 
+#               user's computer.
+#
+#  REQUIREMENTS:  * Google Chrome 31.0 or higher.
+#                 * NodeJS must be installed too, besides this is indicated in the repository
+#                 of serverjs-gossip
+#
+#          BUGS:  ---
+#
+#         NOTES:  * The parameter <peers> must be lower than 16
+#                 * ATENTTION: During the execution some window alerts of the browser could
+#                 appear, it is recommended do not performed any action on them
+#                 * The user must be sure that to type the command 'google-chrome' in a 
+#                 terminal lunch the Chrome browser
+#                 * Any type matching is performed
+#
+#        AUTHOR:  Raziel Carvajal-Gomez (), raziel.carvajal-gomez@inria.fr
+#       COMPANY:  INRIA, Rennes
+#       VERSION:  1.0
+#       CREATED:  27/02/2014 18:14:14 CET
+#      REVISION:  ---
+#===============================================================================
+
+peers=$1
+exeTime=$2
+serverDir=$3
+testDir=$4
+origin=`pwd`
+simLim=3
+chromeStr="--enable-logging --log-level=0 --user-data-dir="
+
+cd $testDir
+testDir=`pwd`
+peersDir="peers"
+mkdir $peersDir
+cd $peersDir
+declare -a chromePids
+
+for (( COUNTER=0; COUNTER<$peers; COUNTER++ )); do
+  peerDir="peer_$COUNTER"
+  mkdir $peerDir
+  google-chrome $chromeStr$peerDir &>/dev/null &
+  chromePids[$COUNTER]=$!
+  sleep 3
+  kill -9 ${chromePids[$COUNTER]}
+done
+
+cd $serverDir
+cd serverjs-gossip/examples
+node server.js 9001 4 >/dev/null &
+nodePid=$!
+
+cd $origin
+for (( COUNTER=0; COUNTER<$peers; COUNTER++ )); do
+  peerDir="peer_$COUNTER"
+  i=$(( $COUNTER % $simLim ))
+  htmFile=$i".html"
+  google-chrome $chromeStr$testDir/$peersDir/$peerDir $htmFile &>/dev/null &
+  chromePids[$COUNTER]=$!
+done
+
+echo "Waiting..."
+sleep $exeTime
+kill -9 $nodePid
+echo -e "\tDONE"
+
+cd $testDir
+resultsDir="results"
+mkdir $resultsDir
+cd $resultsDir
+
+for (( COUNTER=0; COUNTER<$peers; COUNTER++ )); do
+  kill -9 ${chromePids[$COUNTER]}
+  peerDir="peer_$COUNTER"
+  mkdir $peerDir
+  mv ../$peersDir/$peerDir/chrome_debug.log $peerDir
+  cat $peerDir/chrome_debug.log | sed -n '/{*}/p' | sed 's/.*{\(.*\)}.*/\1/' >$peerDir/log
+done
+
+echo "END OF THE EXECUTION"
