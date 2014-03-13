717a24a0b4d8fe81f81e0e74f08bd0800ef2e407
raziel <raziel.carvajal-gomez@inria.fr>
2014-03-13 14:39:27 +0100

    Removing some non-used plugins in grunt and npm files

--- Gruntfile.js
+++ Gruntfile.js
@@ -11,28 +11,28 @@ module.exports = function(grunt) {
         src: 'lib/<%= pkg.name %>.js',
         dest: 'dist/<%= pkg.name %>.min.js'
       }
-    },
-    copy: {
-      main: {
-        files: [
-	        {expand: true, cwd: 'lib', src: '*', dest: 'dist/'}
-	      ]
-      }
-    },
-    karma: {
-      unit: {
-        configFile: 'karma.conf.js'
-      }
     }
+//    copy: {
+//      main: {
+//        files: [
+//	        {expand: true, cwd: 'lib', src: '*', dest: 'dist/'}
+//	      ]
+//      }
+//    },
+//    karma: {
+//      unit: {
+//        configFile: 'karma.conf.js'
+//      }
+//    }
   });
 
   // Load the plugin that provides the "uglify" task.
   grunt.loadNpmTasks('grunt-contrib-uglify');
-  grunt.loadNpmTasks('grunt-contrib-copy');
-  grunt.loadNpmTasks('grunt-karma');
+//  grunt.loadNpmTasks('grunt-contrib-copy');
+//  grunt.loadNpmTasks('grunt-karma');
 
   // Default task(s).
-  grunt.registerTask('default', ['uglify', 'copy']);
-  grunt.registerTask('test', ['karma']);
-
+  grunt.registerTask('default', ['uglify']);
+//  grunt.registerTask('default', ['uglify', 'copy']);
+//  grunt.registerTask('test', ['karma']);
 };
