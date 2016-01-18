376219a9fbc851c74bedda6f7dd916a951dc9db7
Raziel <raziel.carvajal@unine.ch>
2016-01-18 17:32:46 +0100

    The similarity function was improved and now peers' profile could be updated after the experiment started

--- examples/chatApp/confObj.js
+++ examples/chatApp/confObj.js
@@ -1,7 +1,7 @@
 var configurationObj = {
   signalingService: {
     //host: 'localhost',
-    //host: '131.254.213.43',
+    host: '131.254.213.43',
     port: 9990
   },
   gossipAlgos: {
@@ -24,15 +24,21 @@ var configurationObj = {
       // value of each character. If the size of the vectors isn't the
       // same, the defficit is filled with zeros
       similarityFunction: function (a, b) {
-        var maxLength = Math.max(a.length, b.length)
-        console.log('SIMILA: ' + JSON.stringify(a) + '  ' + JSON.stringify(b))
-        var resu = 0
-        for (var i = 0; i < maxLength; i++) {
-          if (typeof a[i] != undefined && typeof b[i] != undefined) {
-            if (a[i] == b[i]) resu += 1
-            else resu += 0.4
-          } else resu += 0.1
+        var bCpy = [], i, there = 0, notThere = 0, indx
+        for (i = 0; i < b.length; i++) bCpy.push(b[i])
+        for (i = 0; i < a.length; i++) {
+          indx = bCpy.indexOf(a[i], 0)
+          if (indx != -1) {
+            there++
+            bCpy[indx] = undefined
+          } else notThere++
         }
+        for (i = 0; i < bCpy.length; i++) {
+          if (typeof bCpy[i] != 'undefined') notThere++
+        }
+        console.log('SIMILA: ' + JSON.stringify(a) + '  ' + JSON.stringify(b))
+        console.log('EVALU: t:' + there + ' nT:' + notThere)
+        var resu = there * (1.0) + notThere * (-0.3)
         console.log('SIMILA RESU: ' + resu)
         return resu
       },
